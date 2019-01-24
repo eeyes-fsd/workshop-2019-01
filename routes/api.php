ac241ea6c456818615b729596bdd05f38c64e614
XuZhixuan <sxxuzhixuan@hotmail.com>
2019-01-24 12:37:56 +0800

    Add Dingo API

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api'
+], function ($api) {
+   //
+});
